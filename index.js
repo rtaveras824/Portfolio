f1eace44d49968361f29fd8ff2b14a0c2f525e4a
Reuben Taveras <rtaveras824@hotmail.com>
2022-08-25 21:02:34 -0400

    Serves index.html file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.send('index.html');
 });
 
 app.listen(port, () => {
