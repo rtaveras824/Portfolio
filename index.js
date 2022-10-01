5749a26fefb6fcdc3e6b61c5493a955e08b6727e
Reuben Taveras <rtaveras824@hotmail.com>
2022-09-30 23:51:53 -0400

    Adding todo list

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
 	res.send('index.html');
 });
 
+app.get('/todo', (req, res) => {
+	res.send('todo/todo-list.html');
+});
+
+app.get('/todo-list', (req, res) => {
+	res.send('todo-list.html');
+});
+
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
 })
